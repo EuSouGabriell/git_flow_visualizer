e44dbc71e87e019bdd9b523d452a2e75022e26d6
Gabriel Santos <84045805+GabrielSantos777@users.noreply.github.com>
2026-06-10 23:00:36 -0300

    feat: adicionar estrutura inicial com componentes e estilos

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="header">
+        <div className="header_title">GitFlow Visualizer</div>
+        <div className="header_subtitle">Estratégias de branching interativas</div>
+        <div className="header_toggle" />
+      </div>
+
+      <div className="terminal_git" />
+    </>
+  );
+}
+
+export default App;
